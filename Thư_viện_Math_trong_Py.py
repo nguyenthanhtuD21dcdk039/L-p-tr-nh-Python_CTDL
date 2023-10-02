bd5ec5cf9038a5ac98b0d3da0d7bd3ffad68e936
nguyenthanhtuD21dcdk039 <nguyenthanhtu12a2@gmail.com>
2023-10-02 13:54:23 +0700

    add Thư_viện_Math_trong_Py.py file

---
+++ Thư_viện_Math_trong_Py.py
@@ -0,0 +1,7 @@
+import math
+
+x = float(input("Nhập vào giá trị của x = "))
+print("|x| = ", math.fabs(x))  # Lấy trị tuyệt đối
+print("sqrt(x) = ", math.sqrt(x))  # Lấy căn bậc 2
+print("ceil(x) = ", math.ceil(x))  # Làm tròn lên giá trị phía trên vd: 4.3=5
+print("floor(x) = ", math.floor(x))  # Làm tròn xuống giá trị phía dưới vd: 4.8=4
